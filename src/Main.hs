aec70b67dcef72cecb44e9088a5758b5e97fd545
pablobuiras <pablobuiras@gmail.com>
2010-04-29 12:20:18 -0300

    A better REPL. Also uses model builder.

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,10 @@ import Formula.Parser (formula)
 import Formula.Pretty
 import Text.ParserCombinators.Parsec
 import Data.Either
+import qualified Data.Map as M
 import System.IO
 import Control.Monad
+import ModelBuilder
 
 pf lf = rights $ map (parse formula "<interactive>") lf
 parseformula f = head $ pf [f]
@@ -20,8 +22,15 @@ repl :: IO ()
 repl = forever $
          do putStr "> "
 	    l <- getLine
-	    if not (null l) then do
-	                           print $ parseformula l  
-			           putStr $ "Computing... "++ show (length $ takeWhile (\x -> not (x==FTrue)) $ step $ parseformula l) ++ " step(s) \nTrue!\n"
-	                    else return ()
+            when (null l) $ return ()
+            case parse formula "<interactive>" l of
+              Left err -> print err
+              Right f -> do putStr "Checking..."
+                            case tauto f of
+                              Counter v ->
+                               do putStr "Formula is not a tautology. Counterexample: "
+                                  mapM_ (putStr . show) (M.toList v)
+                                  putStr "\n"
+                              Tauto -> do putStr "Formula is a tautology. Proving..."
+			                  putStr $ show (length $ takeWhile (\x -> not (x==FTrue)) $ step f) ++ " step(s) \nTrue!\n"
 	    --either print print . parse formula "<interactive>" l  -- use readline later
