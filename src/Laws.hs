c3fbfe59099529de35199dc141adcce2099b1e60
pablobuiras <pablobuiras@gmail.com>
2010-05-20 13:15:15 -0300

    Added test laws to default.laws

--- src/Laws.hs
+++ src/Laws.hs
@@ -69,54 +69,6 @@ testLaws = [
               --(Var "a" :& Var "b", Var "b" :& Var "a"),
 
 	   ]
-{-ordLaws x = case x of 
-                      -- Reglas que SIEMPRE conviene seleccionar primero (reducen estructura)
-                      (Var "a" :== Var "a", FTrue) 	     	    -> 0
-		      (FTrue :== FFalse, FFalse)		    -> 0
-		      (FTrue :== FTrue, FTrue)		    	    -> 0
-		      (Not FTrue, FFalse)			    -> 0
-		      (Not FFalse, FTrue)			    -> 0
-		      (Var "a" :== FTrue, Var "a")		    -> 0
-		      (Var "a" :& FTrue , Var "a")		    -> 0
-		      (Var "a" :& Var "a", Var "a")		    -> 0
-		      (Var "a" :| Var "a", Var "a")		    -> 0
-		      (Var "a" :| FTrue, FTrue)		    	    -> 0
-		      (Var "a" :& FFalse, FFalse)		    -> 0
-		      
-		      -- Reglas que conviene aplicar en 2do lugar
-		      -- Golden rule
-		      ((Var "a" :& Var "b"), Var "a" :== (Var "b" :== (Var "a" :| Var "b"))) -> 10
-		      ((Var "a" :& Var "b") :==  Var "a", Var "b" :== (Var "a" :| Var "b")) -> 1
-		      ((Var "a" :& Var "b") :== (Var "a" :== Var "b"),(Var "a" :| Var "b")) -> 1
-		      ((Var "a" :| Var "b"), Var "a" :== (Var "b" :== (Var "a" :& Var "b"))) -> 10
-		      ((Var "a" :| Var "b") :==  Var "a", Var "b" :== (Var "a" :& Var "b")) -> 1
-		      ((Var "a" :| Var "b") :== (Var "a" :== Var "b"),(Var "a" :& Var "b")) -> 1
-		      
-		      
-		      -- Reglas que conviene aplicar en ultimo lugar
-		      
-		      -- Permutatividad
-                      (Var "a" :== Var "b", Var "b" :== Var "a") -> 1
-		      (Var "a" :& Var "b", Var "b" :& Var "a")   -> 1
-		      (Var "a" :| Var "b", Var "b" :| Var "a")   -> 1
-		      
-		      --(Var "a" :| Var "b", Not ((Not (Var "a")) :& (Not (Var "b")))) -> 2
-		      
-		      -- Asociatividad
-		      (Var "a" :== (Var "b" :== Var "c"), (Var "a" :== Var "b") :== Var "c") -> 1
-		      (Var "a" :& (Var "b" :& Var "c"), (Var "a" :& Var "b") :& Var "c") -> 1
-		      (Var "a" :| (Var "b" :| Var "c"), (Var "a" :| Var "b") :| Var "c") -> 1
-		      
-		      -- Distributidad
-		      (Var "a" :| (Var "b" :== Var "c"), (Var "a" :| Var "b") :== (Var "a" :| Var "c")) ->  1
-		      (Var "a" :| ( (Var "b" :== Var "c") :== (Var "a" :| Var "b") ) , (Var "a" :| Var "c")) -> 1
-		      
-		      
-		      (Var "a" :& (Var "b" :== Var "c"), ((Var "a" :& Var "b") :== (Var "a" :& Var "c")) :== Var "a") -> 1
-		      (Var "a" :& ( (Var "b" :== Var "c") :== (Var "a" :& Var "b") ), (Var "a" :& Var "c") :== Var "a")-> 1  
-		      (Var "a" :& ( (Var "b" :== Var "c") :== (Var "a" :& Var "b") :== (Var "a" :& Var "c") ) , Var "a")-> 1
-		      
--}
 
 testFormula = ( (Var "p" :| (Var "p" :& Var "q")) :== Var "p") 
 testFormulb = ( (Var "p" :& (Var "p" :| Var "q")) :== Var "p") 
