36b6dc443abbb8afc55cccbaaff346f785779b77
Pablo Buiras <pablobuiras@gmail.com>
2011-02-21 16:11:43 -0300

    Parser for new formula representation.

--- src/Formula/Parser.hs
+++ src/Formula/Parser.hs
@@ -9,6 +9,26 @@ import Text.ParserCombinators.Parsec.Language (haskellStyle)
 import Exceptions
 import Control.Exception
 import Data.Typeable
+import Control.Arrow
+
+data TF = TVar String | TFalse | TTrue
+        | TAnd TF TF | TOr TF TF | TEq TF TF
+        | TImpl TF TF | TConseq TF TF | TEqual TF TF
+        | TNot TF
+          deriving (Eq, Show)
+
+toFormula :: TF -> Formula
+toFormula f = normalize $ case f of
+  TVar s -> Var s
+  TFalse -> FFalse
+  TTrue -> FTrue
+  TAnd f1 f2 -> FAnd [toFormula f1, toFormula f2]
+  TOr f1 f2 -> FOr [toFormula f1, toFormula f2]
+  TEq f1 f2 -> FEquiv [toFormula f1, toFormula f2]
+  TImpl f1 f2 -> toFormula f1 :=> toFormula f2
+  TConseq f1 f2 -> toFormula f1 :<= toFormula f2
+  TEqual f1 f2 -> toFormula f1 := toFormula f2
+  TNot f -> Not (toFormula f)
 
 -- Lexer
 lexer = P.makeTokenParser
@@ -22,23 +42,23 @@ identifier = P.identifier lexer
 reserved = P.reserved lexer
 reservedOp = P.reservedOp lexer
 
-
 -- Parser
-var = fmap Var identifier <?> "variable"
-table = [[Prefix ((reservedOp "~" >> return Not) <?> "negation")],
-         [{-op "/\\" (:&) AssocLeft, op "\\/" (:|) AssocLeft-}],
-         [op "=>" (:=>) AssocRight, op "<=" (:<=) AssocLeft],
-         [{-op "==" (:==) AssocLeft,-} op "=" (:=) AssocNone]]
+var = fmap TVar identifier <?> "variable"
+table = [[Prefix ((reservedOp "~" >> return TNot) <?> "negation")],
+         [op "/\\" TAnd AssocLeft, op "\\/" TOr AssocLeft],
+         [op "=>" TImpl AssocRight, op "<=" TConseq AssocLeft],
+         [op "==" TEq AssocLeft, op "=" TEqual AssocNone]]
         where op s f =
                   Infix ((reservedOp s >> return f) <?> "operator")
 
-formula = buildExpressionParser table term
-term = parens formula
+formula' = buildExpressionParser table term
+term = parens formula'
        <|> var
-       <|> (reserved "true" >> return FTrue)
-       <|> (reserved "false" >> return FFalse)
+       <|> (reserved "true" >> return TTrue)
+       <|> (reserved "false" >> return TFalse)
        <?> "proposition"
 
+formula = fmap toFormula formula'
 
 lawPrim = do f <- formula
              reservedOp "->"
